c9786acbea5db643c81c5dbca6e6c1e02493e99c
AliDarwish732 <alllout732@gmail.com>
2025-07-01 01:13:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// رسالة ترحيب عند فتح الصفحة
+window.onload = function() {
+  console.log("Welcome to Ali Ahmed's CV Website");
+};
+
+// تفعيل الوضع الليلي
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
